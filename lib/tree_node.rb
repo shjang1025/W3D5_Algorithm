37fdb52253e8f65c51d46776c7832505d41d51d9
shjang1025 <shjang1025@gmail.com>
2024-01-26 13:12:08 -0800

    define intialize and parent= method

--- lib/tree_node.rb
+++ lib/tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
+    attr_reader :value,:parent,:children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(new_parent)
+        if new_parent == nil
+            @parent = new_parent
+        else 
+            if parent == nil
+                @parent = new_parent
+                new_parent.children << self
+            else 
+                parent.children.delete(self)
+                @parent = new_parent
+                new_parent.children << self
+            end
+        end
+
+    end
   
 end
