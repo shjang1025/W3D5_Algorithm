3089226eb3c2549a7f19c17f09ad3b69be0069f6
acheung-94 <acheung@ucdavis.edu>
2024-01-29 14:25:45 -0800

    define build_move_tree method

--- knights-travails/knight_path_finder.rb
+++ knights-travails/knight_path_finder.rb
@@ -1,6 +1,9 @@
 require_relative '../lib/tree_node.rb'
+require 'byebug'
 class KnightPathFinder #chess board max row/col index = 7,7
     MOVES = [[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]]
+    attr_reader :root_node, :considered_positions
+
     def initialize(starting_pos)
        @root_node = PolyTreeNode.new(starting_pos) #<= value of node
        @considered_positions = [starting_pos]
@@ -10,7 +13,7 @@ class KnightPathFinder #chess board max row/col index = 7,7
         x,y = start_pos
         valid_moves = []
         MOVES.each do |p_move|
-            if x + p_move.first < 8 && y + p_move.last < 8
+            if x + p_move.first < 8 && x + p_move.first >= 0 && y + p_move.last < 8 && y + p_move.last >= 0
                 valid_moves << [x + p_move.first, y + p_move.last]
             end
         end
@@ -22,13 +25,29 @@ class KnightPathFinder #chess board max row/col index = 7,7
 
         selected = all_new_moves.select {|position| !@considered_positions.include?(position)}
         @considered_positions.push(*selected)
-        return selected
+        return selected #a 2D array
     end
-    
+    #debugger   
+    def build_move_tree # [ (all move 1 nodes), (all move 2 nodes), (...)]
+        queue = [root_node] #queue contains instances of PolyTreeNode (can access .value)
+        until queue.empty?
+            current_node = queue.shift
+              
+            next_moves = new_move_positions(current_node.value).map {|ele| PolyTreeNode.new(ele) } #create new polytree nodes, usinv current PTN.value
+            next_moves.each do |child_move|
+                current_node.add_child(child_move)
+                # p current_node.children
+                queue << child_move
+            end
+            
+        end
+    end
 
 end
 
-test = KnightPathFinder.new([2,4])
-p test
-p KnightPathFinder.valid_moves([2,4])
-p test.new_move_positions([3,2])
+if __FILE__ == $PROGRAM_NAME
+test = KnightPathFinder.new([0,0])
+p test #is a node 
+p test.build_move_tree
+p test.considered_positions.length
+end
