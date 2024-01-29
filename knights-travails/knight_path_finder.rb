8b484046308d84a1af0e6cb85e5eb1a8610242cf
shjang1025 <shjang1025@gmail.com>
2024-01-29 10:11:29 -0800

    define new_move_position method

--- knights-travails/knight_path_finder.rb
+++ knights-travails/knight_path_finder.rb
@@ -3,6 +3,7 @@ class KnightPathFinder #chess board max row/col index = 7,7
     MOVES = [[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1],[-1,2]]
     def initialize(starting_pos)
        @root_node = PolyTreeNode.new(starting_pos) #<= value of node
+       @considered_positions = [starting_pos]
     end
 
     def self.valid_moves(start_pos) #new positions from a given position
@@ -15,9 +16,19 @@ class KnightPathFinder #chess board max row/col index = 7,7
         end
         valid_moves
     end
+    #available move positions that are not in the previous position. 
+    def new_move_positions(another_starting_pos)
+        all_new_moves = KnightPathFinder.valid_moves(another_starting_pos)
+
+        selected = all_new_moves.select {|position| !@considered_positions.include?(position)}
+        @considered_positions.push(*selected)
+        return selected
+    end
+    
 
 end
 
 test = KnightPathFinder.new([2,4])
 p test
 p KnightPathFinder.valid_moves([2,4])
+p test.new_move_positions([3,2])
